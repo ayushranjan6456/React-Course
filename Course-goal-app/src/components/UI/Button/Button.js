9e02c9bbeb57952095a746ae46864588a3e2b431
Ayush Ranjan <ayushranjan6456@gmail.com>
2021-05-23 23:35:00 +0530

    Media Query

--- Course-goal-app/src/components/UI/Button/Button.js
+++ Course-goal-app/src/components/UI/Button/Button.js
@@ -15,13 +15,18 @@
 import styled from "styled-components";
 
 const Button = styled.button`
-    font: inherit;
-    padding: 0.5rem 1.5rem;
-    border: 1px solid #8b005d;
-    color: white;
-    background: #8b005d;
-    box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-    cursor: pointer;
+  width: 100%;
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+
+  @media (min-width: 768px){
+    width: auto;
+  }
 
   &:focus {
     outline: none;
