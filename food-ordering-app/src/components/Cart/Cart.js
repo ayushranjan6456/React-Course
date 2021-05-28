0abe494f387970020453ce1787a567622d309f83
Ayush Ranjan <ayushranjan6456@gmail.com>
2021-05-28 23:16:52 +0530

    Error Modal

---
+++ food-ordering-app/src/components/Cart/Cart.js
@@ -0,0 +1,27 @@
+import classes from "./Cart.module.css";
+
+const Cart = () => {
+  const CartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "c2", name: "Sushi", amount: 2, price: "12.99" }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div>
+      {CartItems}
+      <div>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
---
+++ food-ordering-app/src/components/UI/Modal.js
@@ -0,0 +1,7 @@
+import classes from './Modal.module.css'
+
+const Modal = (props) => {
+ return
+}
+
+export default  Modal
