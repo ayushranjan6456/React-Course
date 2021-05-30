b1add6591cb4708c570386f03854198585942c7a
Ayush Ranjan <ayushranjan6456@gmail.com>
2021-05-30 16:50:06 +0530

    useMemo, memo, useCallback added

---
+++ useMemo/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState, useCallback, useMemo } from 'react';
+
+import './App.css';
+import DemoList from './components/Demo/DemoList';
+import Button from './components/UI/Button/Button';
+
+function App() {
+  const [listTitle, setListTitle] = useState('My List');
+
+  const changeTitleHandler = useCallback(() => {
+    if(listTitle === 'Mylist')
+      setListTitle('New Title');
+    else 
+      setListTitle('Mylist');
+  }, [listTitle]);
+
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
+
+  return (
+    <div className="app">
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Toggle List Title</Button>
+    </div>
+  );
+}
+
+export default App;
