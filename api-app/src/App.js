349c897f8f965f7a2ebbbf407478f4399f126a27
Ayush Ranjan <ayushranjan6456@gmail.com>
2021-05-30 19:30:15 +0530

    Using Async function

--- api-app/src/App.js
+++ api-app/src/App.js
@@ -6,24 +6,45 @@ import "./App.css";
 function App() {
   const [moviesList, setMoviesList] = useState([]);
 
-  const fetchMovieHandler = () => {
-    fetch("https://swapi.dev/api/films/", { method: "GET" })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const formattedMoviesList = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            releaseDate: movieData.release_date,
-            openingText: movieData.opening_crawl,
-          };
-        });
-        console.log(formattedMoviesList);
-        setMoviesList(formattedMoviesList);
-      })
-      .catch((error) => console.log(error));
+
+  //Without async function using then
+
+
+  // const fetchMovieHandler = () => {
+  //   fetch("https://swapi.dev/api/films/", { method: "GET" })
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       const formattedMoviesList = data.results.map((movieData) => {
+  //         return {
+  //           id: movieData.episode_id,
+  //           title: movieData.title,
+  //           releaseDate: movieData.release_date,
+  //           openingText: movieData.opening_crawl,
+  //         };
+  //       });
+  //       console.log(formattedMoviesList);
+  //       setMoviesList(formattedMoviesList);
+  //     })
+  //     .catch((error) => console.log(error));
+  // };
+
+
+  //Using Async function
+  async function fetchMovieHandler() {
+    const response = await fetch("https://swapi.dev/api/films/", { method: "GET" });
+    const data = await response.json();
+    const formattedMoviesList = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        releaseDate: movieData.release_date,
+        openingText: movieData.opening_crawl,
+      };
+    });
+    console.log(formattedMoviesList);
+    setMoviesList(formattedMoviesList);
   };
 
   return (
