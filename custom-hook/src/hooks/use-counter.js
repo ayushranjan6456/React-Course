91368ce0434aed7b5ace4bb1b59627f013b723ed
Ayush Ranjan <ayushranjan6456@gmail.com>
2021-06-01 21:57:54 +0530

    custom hook logic

--- custom-hook/src/hooks/use-counter.js
+++ custom-hook/src/hooks/use-counter.js
@@ -1,12 +1,16 @@
 import { useState, useEffect } from "react";
 
 //hard rule function name must start with 'use'
-const useCounter = () => {
+const useCounter = (forwards = true) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      if (forwards) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
